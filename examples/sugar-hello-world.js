936ef3b4346c19d26c15bb08f8452678959cf68a
isaacs <i@izs.me>
2010-01-18 02:10:22 -0800

    Working hello world type stuff with basic routing.

--- examples/sugar-hello-world.js
+++ examples/sugar-hello-world.js
@@ -1,22 +1,21 @@
-require.paths.unshift(require("path").join(__filename, "../../lib"));
-var mojito = require("mojito");
+var mojito = require("./../lib/mojito");
 
 mojito
   ( function () { this.sendHeader(200, {"content-type":"text"}).next() })
-  ( function test () { return this.request.url.match(/^\/hello/) },
-    function () { this.sendBody("hello").next() })
-  ( function test () { return !this.request.url.match(/^\/hello/) },
-    function () { this.sendBody("yorp").next() })
-  ( function () { this.sendBody(", world") } )
+  ( /^\/hello/, function () { this.sendBody("hello").next() })
+  ( { test : /^(?!\/hello)/,
+    action : function () { this.sendBody("yorp").next() } })
+  ( function () { this.sendBody(", world").next() } )
+  ( function () { this.done() } )
   .listen(8000, "localhost");
 
 mojito
   (function () { this.sendHeader(200, {"content-type":"text/html"}).next() } )
   (/^\/foo/, mojito
     (function () { this.sendBody("foo!").next() })
-    (function () { this.finish().done() }))
+    (function () { this.done() }))
   (/^\/bar/, mojito
     (function () { this.sendBody("<html><title>bar<p>BAR!<br>").next() }))
   (function () { this.sendBody("not foo").next() })
-  (function () { this.finish().done() })
+  (function () { this.finish() })
   .listen(8080, "localhost");
